3d9c67ab10c55c2f44d1a3bfe2a364ba939ac504
nisitasubudhi <162950721+nisitasubudhi@users.noreply.github.com>
2024-06-27 11:58:59 +0530

    listener.tf

--- listener.tf
+++ listener.tf
@@ -1,4 +1,12 @@
-
+resource "aws_lb_listener" "external-alb" {
+  load_balancer_arn = aws_lb.external-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target-elb.arn
+  }
+}
 listener_arn = aws_lb_listener.external-alb.arn
   priority     = 100
   action {
